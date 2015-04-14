0a5053661ba1de9943d22cbbd5f503623a714e95
crandt <crandt@uw.edu>
2015-04-14 11:18:49 -0700

    Initial Theme Configurations

--- content/themes/DRIVE2016/library/admin.php
+++ content/themes/DRIVE2016/library/admin.php
@@ -15,7 +15,7 @@ function disable_default_dashboard_widgets() {
 	remove_meta_box('dashboard_incoming_links', 'dashboard', 'core');  // Incoming Links Widget
 	remove_meta_box('dashboard_plugins', 'dashboard', 'core');         // Plugins Widget
 
-	// remove_meta_box('dashboard_quick_press', 'dashboard', 'core');  // Quick Press Widget
+	remove_meta_box('dashboard_quick_press', 'dashboard', 'core');     // Quick Press Widget
 	remove_meta_box('dashboard_recent_drafts', 'dashboard', 'core');   // Recent Drafts Widget
 	remove_meta_box('dashboard_primary', 'dashboard', 'core');         //
 	remove_meta_box('dashboard_secondary', 'dashboard', 'core');       //
